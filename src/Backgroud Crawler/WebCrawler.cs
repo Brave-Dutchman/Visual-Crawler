01b1ed84c687b5633bd222b48267233fc0711985
Thijs <mreeringh@hotmail.com>
2016-03-21 15:02:00 +0100

    updated webcrawler

--- src/Backgroud Crawler/WebCrawler.cs
+++ src/Backgroud Crawler/WebCrawler.cs
@@ -8,60 +8,101 @@ namespace Backgroud_Crawler
 {
     public class WebCrawler
     {
+        private const string StartUrl = "http://www.insidegamer.nl/";
+        
         private readonly int _number;
-        private readonly List<string> _foundLinks;
+
+        private readonly List<string> _links;
+        private readonly List<ALink> _foundText;
 
         public WebCrawler(int number)
         {
             _number = number;
-            _foundLinks = new List<string>();
+
+            _links = new List<string>();
+            _foundText = new List<ALink>();
         }
 
         public void Run()
         {
-            Crawler("https://en.wikipedia.org/wiki/Special:Random");
+            int level = 0;
+            Crawler(StartUrl, level);
+
+            while (true)
+            {
+                level++;
+
+                ALink[] linsk = GetByLevels(level);
+
+                foreach (ALink link in linsk)
+                {
+                    Crawler(link.Link, level);
+                }
+            }
         }
 
-        private void Crawler(string url)
+        private void Crawler(string webUrl, int level)
         {
-            WebRequest myWebRequest = WebRequest.Create(url);
-            using (WebResponse myWebResponse = myWebRequest.GetResponse())
+            try
             {
-                using (Stream streamResponse = myWebResponse.GetResponseStream())
+                WebRequest myWebRequest = WebRequest.Create(webUrl);
+                using (WebResponse myWebResponse = myWebRequest.GetResponse())
                 {
-                    using (StreamReader sreader = new StreamReader(streamResponse))
+                    using (Stream streamResponse = myWebResponse.GetResponseStream())
                     {
-                        string rstring = sreader.ReadToEnd(); //reads it to the end
-
                         string header = myWebResponse.ResponseUri.Scheme + "://" + myWebResponse.ResponseUri.Host;
+                        Console.WriteLine($"{_number}:{level}:{myWebResponse.ResponseUri}\n");
 
-                        GetNewLinks(rstring, header);
+                        using (StreamReader sreader = new StreamReader(streamResponse))
+                        {
+                            string content = sreader.ReadToEnd(); //reads it to the end
+                            Regex regexLink = new Regex("(?<=<a\\s*?href=(?:'|\"))[^'\"]*?(?=(?:'|\"))");
+
+                            foreach (object match in regexLink.Matches(content))
+                            {
+                                string url = match.ToString();
+                                if (url.StartsWith("#") || Path.HasExtension(url) || url.Equals("/")) continue;
+
+                                if (!url.StartsWith("http"))
+                                {
+                                    url = header + url;
+                                }
+
+                                if (_links.Contains(url)) continue;
+
+                                _foundText.Add(new ALink(level + 1, url));
+                                _links.Add(url);
+                            }
+                        }
                     }
                 }
             }
+            catch (Exception e)
+            {
+                Console.WriteLine($"\t{webUrl}:{e.Message}");
+            }
         }
 
-        private void GetNewLinks(string content, string header)
+        private ALink[] GetByLevels(int level)
         {
-            Regex regexLink = new Regex("(?<=<a\\s*?href=(?:'|\"))[^'\"]*?(?=(?:'|\"))");
+            return _foundText.FindAll(x => x.Level == level).ToArray();
+        }
+    }
 
-            foreach (object match in regexLink.Matches(content))
-            {
-                string url = match.ToString();
-                if (url.StartsWith("#") || url.Contains(":") || url.Contains(".")) continue;
+    public class ALink
+    {
+        public ALink(int level, string link)
+        {
+            Level = level;
+            Link = link;
+        }
 
-                if (!url.StartsWith("http"))
-                {
-                    url = header + url;
-                }
+        public int Level { get; set; }
+        public string Link { get; set; }
 
-                if (!_foundLinks.Contains(url))
-                {
-                    _foundLinks.Add(url);
-                    Console.WriteLine($"{_number}:{_foundLinks.Count}:{url}\n");
-                    Crawler(url);
-                }
-            }
+        public override string ToString()
+        {
+            return Link;
         }
     }
 }

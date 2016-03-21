3e7b3c43aa7352dcb06bec6000e754fae73465ef
Thijs <mreeringh@hotmail.com>
2016-03-21 09:06:49 +0100

    First Commit

---
+++ Visual Crawl/Backgroud Crawler/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backgroud_Crawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
